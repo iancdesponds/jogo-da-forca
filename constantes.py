6d04edd75d98528055b262d366f303805739bf61
iancdesponds <iancdesponds@gmail.com>
2023-09-27 01:50:18 -0300

    feat: added constantes file

---
+++ constantes.py
@@ -0,0 +1,37 @@
+# Colors
+
+white = (255, 255, 255)
+black = (0, 0, 0)
+gray = (128, 128, 128)
+
+# Screen
+
+screen_width = 800
+screen_height = 500
+screen_size = (screen_width, screen_height)
+
+# Palavras
+
+words = [
+    "banana", "cachorro", "guitarra", "bicicleta", "computador", 
+    "chocolate", "elefante", "floresta", "fruta", "garrafa",
+    "hamster", "hamburguer", "igreja", "inverno", "jornal",
+    "lapis", "limonada", "macaco", "melancia", "montanha",
+    "navio", "oculos", "papagaio", "pipoca", "poste",
+    "raposa", "refrigerante", "rosa", "sorvete", "tartaruga",
+    "telefone", "tigre", "tomate", "uva", "violao",
+    "xadrez", "zebra", "abacaxi", "arvore", "barco",
+    "bolacha", "cadeira", "carro", "castelo", "cavalo",
+    "cidade", "cobertor", "dinossauro", "espelho", "foguete",
+    "futebol", "girafa", "helicopetero", "lapis", "leao",
+    "lua", "mala", "moto", "nuvem", "ovelha",
+    "parque", "passaro", "peixe", "pinguim", "planta",
+    "praia", "rato", "relogio", "sapato", "sol",
+    "telefone", "trem", "vulcao", "zumbi", "fantasma",
+    "floresta", "vento", "teclado", "espada", "lanterna",
+    "aviao", "aviacao", "cadeado", "grande", "coracao",
+    "chave", "boneco", "sapo", "abacate", "dinheiro",
+    "melancia", "cachecol", "escova", "cimento", "revolver",
+    "cachorro", "fogao", "meia", "olho", "terra",
+    "janela", "carteira", "formiga", "tesoura", "prancha"
+]
